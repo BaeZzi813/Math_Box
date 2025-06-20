b0b38908a8117802e187b0f3c64beb0fb82cef60
BaeZzi813 <woehd8252@gmail.com>
2025-06-20 18:12:04 +0900

    Add say_hello function

--- calculator.py
+++ calculator.py
@@ -5,8 +5,11 @@ def add(a, b):
 def subtract(a, b):
   return a-b
 
-def multiply(a, b);
+def multiply(a, b):
   return a*b
 
-def divide_new(a, b);
+def divide_new(a, b):
   return a/b
+
+def say_hello():
+  print("hello")
